76afbf057bc08901232c244ca34c3d8a04a3ee50
Francisco Robles <frobles@twilio.com>
2023-10-18 09:59:33 -0700

    adding comments, some minor change to code

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function Board({ xIsNext, squares, onPlay, setStatus }) {
 
   let [status, highlights] = setStatus(squares);
 
+  // buildBoard generates the game's board using two for-loops. It also handles the winner's final move, highlighting the squares.
   function buildBoard() {
     const numRows = 3;
     const numCols = 3;
@@ -34,6 +35,7 @@ function Board({ xIsNext, squares, onPlay, setStatus }) {
       board.push(<div key={"row-"+row+1} className="board-row"></div>)
       for (let col = 0; col < numCols; col++) {
         const squareNum = col + 3*row;
+        // check if highlights is not null, and verify the hightlights contain the index squareNum
         if (highlights && highlights.includes(squareNum)) {
           board.push(<Square key={"square-"+(squareNum+1)} className={"square-winner"} value={squares[squareNum]} onSquareClick={() => handleClick(squareNum)} />);
         } else {
@@ -57,6 +59,7 @@ export default function Game() {
   const [currMovePos, setCurrMovePos] = useState([Array(9).fill(null)]);
 
   function handlePlay(nextSquares) {
+    // We use currMovePos array to store the coordinates of every move.
     let previousHistory = history[currentMove]
     for (let pos = 0; pos < nextSquares.length; pos++) {
       if (previousHistory[pos] !== nextSquares[pos]) {
@@ -72,6 +75,9 @@ export default function Game() {
     setCurrentMove(nextMove);
   }
 
+  /* setStatus return a string based on current game's status (winner, on going game or Draw), 
+     and a list of the squares' indexes that caused the win, if there is a winner.
+  */
   function setStatus(squares) {
     const winner = calculateWinner(squares);
     let status;
@@ -87,22 +93,30 @@ export default function Game() {
     return [status, highlights];
   }
 
-  let moves = history.map((squares, move) => {
-    let description;
+  // setPosition takes a move value an returns the move's coordinates in the format (row, col).
+  function setPosition(move) {
     let position = "";
     if (currMovePos.length > 0) {
-      position = ", (" + currMovePos[move][0] + ", " + currMovePos[move][1] + ")";
+      position = "(" + currMovePos[move][0] + ", " + currMovePos[move][1] + ")";
     }
+    return position
+  }
+
+  let moves = history.map((squares, move) => {
+    let description;
+    let position = setPosition(move);
+
     if (move > 0) {
+      // We check if a move is the currentMove, and return a string instead of using a button.
       if (move === currentMove) {
-        description = "You are at move #" + move + position;
+        description = "You are at move #" + move + ", Position: " + position;
         return (
           <div key={move}>
             <div className="description">{description}</div>   
           </div>
         );
       } else {
-        description = "Go to move #" + move + position;
+        description = "Go to move #" + move + ", Position: " + position;
       }
     } else {
       description = "Go to game start";
@@ -115,10 +129,12 @@ export default function Game() {
     );
   });
 
+  // sortMoves flips the boolean isDescending to change the moves history's order.
   function sortMoves(isDescending) {
     setIsDescending(!isDescending);
   }
 
+  // checkOrder checks if isDescending is false, and arranges moves in ascending order.
   function checkOrder(isDescending) {
     if (!isDescending) {
       moves.reverse();
@@ -126,6 +142,7 @@ export default function Game() {
     return moves;
   }
 
+  // Toggle Button used to sort moves list in either ascending or descending order. 
   let toggleButton = "Sort in descending order:";
   if (isDescending) {
     toggleButton = "Sort in ascending order:";
@@ -162,6 +179,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      // returns winner, and a list which contains the indexes of the winner squares.
       return [squares[a], [a, b, c]];
     }
   }
